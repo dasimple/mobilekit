f411e2a802d7154f4d3bd06c32a220af2962d5f4
dasimple <dasimple.com@gmail.com>
2018-06-13 02:58:33 +0300

    added key values collection, getter and setter
    just code

---
+++ Unity/mobilekit/Assets/Plugins/MobileKit/Storing/Collections/KeyValueCollection.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using MobileKit.Storing;
+using MobileKit.Storing.Collections;
+
+namespace WaterSlap.Collections
+{
+	public sealed class KeyValueCollection : VirtualCollection
+	{
+		public KeyValueCollection() : base("keys")
+		{
+
+		}
+
+		public string Get(string name)
+		{
+			return Get(name, "value");
+		}
+
+		public bool Set(string name, string value)
+		{
+			if(Set(name, "value", value) == 0)
+			{
+				StoreDictionary values = Bake(name);
+				values.Add("value", value);
+				return Add(values) == 1;
+			}
+			return true;
+		}
+
+		protected override void AllocateColumns()
+		{
+			base.AllocateColumns();
+			columns.Add("value");
+		}
+	}
+}
---
+++ Unity/mobilekit/Assets/Plugins/MobileKit/Storing/KeyValues.cs
@@ -0,0 +1,91 @@
+﻿using WaterSlap.Collections;
+
+namespace WaterSlap
+{
+	public static class KeyValues
+	{
+		private static KeyValueCollection singleton = null;
+
+		private static KeyValueCollection collection
+		{
+			get
+			{
+				if(singleton == null)
+				{
+					singleton = new KeyValueCollection();
+				}
+				return singleton;
+			}
+		}
+
+		/* Get */
+
+		public static string Get(string name, string fallback = null)
+		{
+			string value = collection.Get(name);
+			if(value == null)
+			{
+				value = fallback;
+			}
+			return value;
+		}
+
+		public static int GetAsInt(string name, int fallback = 0)
+		{
+			int value = fallback;
+			int.TryParse(Get(name), out value);
+			return value;
+		}
+
+		public static long GetAsLong(string name, long fallback = 0)
+		{
+			long value = fallback;
+			long.TryParse(Get(name), out value);
+			return value;
+		}
+
+		public static float GetAsFloat(string name, float fallback = 0f)
+		{
+			float value = fallback;
+			float.TryParse(Get(name), out value);
+			return value;
+		}
+
+		public static bool GetAsBool(string name, bool fallback = false)
+		{
+			string value = Get(name);
+			if(value == null)
+			{
+				return fallback;
+			}
+			return value == "1";
+		}
+
+		/* Set */
+
+		public static bool Set(string name, string value)
+		{
+			return collection.Set(name, value);
+		}
+
+		public static bool Set(string name, int value)
+		{
+			return Set(name, value + "");
+		}
+
+		public static bool Set(string name, long value)
+		{
+			return Set(name, value + "");
+		}
+
+		public static bool Set(string name, float value)
+		{
+			return Set(name, value + "");
+		}
+
+		public static bool Set(string name, bool value)
+		{
+			return Set(name, value ? "1" : "0");
+		}
+	}
+}
